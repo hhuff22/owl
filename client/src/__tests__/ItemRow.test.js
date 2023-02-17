e0311612cae97a6fc8b0e82911bffbd9b9a7d35a
herman huff <herman.huff@fdmgroup.com>
2023-02-17 09:27:34 -0500

    ItemRow test updated

--- client/src/__tests__/ItemRow.test.js
+++ client/src/__tests__/ItemRow.test.js
@@ -5,11 +5,10 @@ import { render, screen, fireEvent } from '@testing-library/react';
 const dummyItem = {
     id: 9,
     name: "dummy item name",
-    stock: 10,
+    stock: 5,
     cost: 1,
     capacity: 7
 };
-
 const dummyLowStock = {
     data: [{
         name: "dummy item name",
@@ -17,7 +16,6 @@ const dummyLowStock = {
     }],
     loading: false
 };
-
 const dummyLowStockTwo = {
     data: [{
         name: "dummy item name 2",
@@ -39,40 +37,55 @@ describe("Item Row", () => {
         expect(asFragment()).toMatchSnapshot();
     });
 
-    test("should handle change correctly", () => {
-        const expectedSetLowStockParam = {data: [{cost: -3, name: "dummy item name"}], loading: false};
+    test("should handle change correctly when low stock name = item name", () => {
+        const expectedSetLowStockParam = {data: [{cost: 2, name: "dummy item name"}], loading: false};
         const props = {
             item: dummyItem,
             lowStock: dummyLowStock,
             setLowStock: mockSetLowStock
         };
-        const setup = () => {
-            const utils = render(<ItemRow {...props} />);
-            const input = screen.getByTestId("item-row-9");
-            return {input, ...utils};
-        };
-        const { input } = setup();
-        expect(input).toBeTruthy();
+
+        render(<ItemRow {...props} />);
+        const input = screen.getByTestId("item-row-9");
         fireEvent.change(input, {target: {value: 2}});
+
+        expect(input).toBeTruthy();
         expect(mockSetLowStock).toHaveBeenCalledTimes(1);
         expect(mockSetLowStock).toHaveBeenCalledWith(expectedSetLowStockParam);
         mockSetLowStock.mockReset();
     });
 
-    test("should not call SetLowStock", () => {
+    test("should handle change correctly when low stock name != item name", () => {
+        const expectedSetLowStockParam = {data: [{cost: 5, name: "dummy item name 2"}], loading: false}
         const props = {
             item: dummyItem,
             lowStock: dummyLowStockTwo,
             setLowStock: mockSetLowStock
         };
-        const setup = () => {
-            const utils = render(<ItemRow {...props} />);
-            const input = screen.getByTestId("item-row-9");
-            return {input, ...utils};
-        };
-        const { input } = setup();
-        expect(input).toBeTruthy();
+
+        render(<ItemRow {...props} />);
+        const input = screen.getByTestId("item-row-9");
         fireEvent.change(input, {target: {value: 2}});
-        expect(mockSetLowStock).not.toHaveBeenCalled();
+
+        expect(input).toBeTruthy();
+        expect(mockSetLowStock).toHaveBeenCalledTimes(1);
+        expect(mockSetLowStock).toHaveBeenCalledWith(expectedSetLowStockParam);
+    });
+
+    test("should handle change correctly when restock amount entered is < 0", () => {
+        const expectedSetLowStockParam = {data: [{cost: 0, name: "dummy item name"}], loading: false}
+        const props = {
+            item: dummyItem,
+            lowStock: dummyLowStock,
+            setLowStock: mockSetLowStock
+        };
+
+        render(<ItemRow {...props} />);
+        const input = screen.getByTestId("item-row-9");
+        fireEvent.change(input, {target: {value: -3}});
+
+        expect(input).toBeTruthy();
+        expect(mockSetLowStock).toHaveBeenCalledTimes(1);
+        expect(mockSetLowStock).toHaveBeenCalledWith(expectedSetLowStockParam);
     });
 })
